7871cc0c4f61d72064d9ba6ec93fc0912086bdd9
Chantal Ramirez <ramirez_yessica@ucol.mx>
2020-09-12 11:12:21 -0500

    archivos para subir archivos

--- controllers/destinations.api.js
+++ controllers/destinations.api.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const destinationsModel = require('../models/destinations');
 const {authenticate} = require ('../utils/security')
-
+const { uploadFileToS3 } = require('../lib/fileUploaders');
+const uuid = require('uuid');
+const path = require('path');
 
 router.get('/',authenticate, async function(req, res, next) {
     try{
@@ -25,15 +27,24 @@ router.get('/:id/show',async (req, res, next) => {
   }),
 
   
-  router.post('/create', async(req, res) => {
-   try{
-       const destination = req.body;
-       await destinationsModel.create(destination);
-       res.status(201).json({ sucess:true });
-   }catch (err){
-       res.status(500).json({error: err.message});
-   }
-   }),
+  router.post('/create', authenticate, async (req, res) => {
+    try {
+      const destination = req.body;
+      if(req.files) {
+        let image = req.files.image;
+        const fileName = image.name;
+        const generatedFileName = `${uuid.v4()}${path.extname(fileName)}`;
+        const filePathAndName = `./public/images/${generatedFileName}`;
+        await image.mv(filePathAndName);
+        const imageUrl = await uploadFileToS3(filePathAndName, generatedFileName);
+        destination.image = imageUrl;
+      }
+      await destinationsModel.create(destination);
+      res.status(201).json({ sucess: true });
+    } catch(err) {
+      res.status(500).json({ error: err.message });
+    }
+  }),
 
    //Actualizar la información de un destino
     router.put('/:id/save', async (req, res) => {
